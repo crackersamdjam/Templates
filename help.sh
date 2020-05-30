387511e0c2cc413455a38896824b82f5c37b9f0e
Eric <crackersamdjam@gmail.com>
2020-05-30 18:34:23 -0400

    .

--- help.sh
+++ help.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-g++ ./fast.cpp --std=c++14 -O2 -o fast
-g++ ./slow.cpp --std=c++14 -O2 -o slow
-g++ ./gen.cpp --std=c++14 -O2 -o gen
+g++ oly19practice27.cpp --std=c++14 -O2 -o fast || { echo 'fast comp failed'; exit 1;}
+g++ slow.cpp --std=c++14 -w -O2 -o slow || { echo 'slow comp failed'; exit 1;}
+g++ gen.cpp --std=c++14 -O2 -o gen || { echo 'gen comp failed'; exit 1;}
 
-for((i=1;i<=1000;++i)); do
+for((i=1;i<=100;++i)); do
     echo $i
     ./gen $i > stressin
     ./fast < stressin > out1

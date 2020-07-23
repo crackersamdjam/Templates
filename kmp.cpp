aa965c91d082e12d46ad7f85d0475885f8c2498d
Eric <crackersamdjam@gmail.com>
2020-07-23 18:03:14 -0400

    Fix tmp

--- kmp.cpp
+++ kmp.cpp
@@ -1,31 +1,42 @@
 #include <bits/stdc++.h>
+#define sz(x) ((int)(x).size())
 using namespace std;
 
-string s, t;
-vector<int> pre;
-
-int main(){;
-	while(getline(cin, s)){
-		getline(cin, t);
-		
-		int sub = s.size();
-		s = s+"#"+t;
-		int n = s.size();
-		
-		pre.resize(n);
-		pre[0] = 0;
-		for(int i = 1; i < s.size(); i++){
-			int j = pre[i-1];
-			while(j and s[i] != s[j])
-				j = pre[j-1];
-			if(s[i] == s[j])
-				j++;
-			pre[i] = j;
-			if(i-sub-sub >= 0 and pre[i] == sub)
-				cout<<i-sub-sub<<' ';
-		}
-		cout<<'\n';
+// pi[i] = length of longest suffix (ending at i) with the same prefix as s
+vector<int> kmp(string s){
+	vector<int> pi(sz(s));
+	pi[0] = 0;
+	for(int i = 1; i < sz(s); i++){
+		int j = pi[i-1];
+		while(j and s[i] != s[j])
+			j = pi[j-1];
+		if(s[i] == s[j])
+			j++;
+		pi[i] = j;
 	}
+	return pi;
+}
+
+vector<int> kmp2(string s, const string &t){
+	int sub = sz(s);
+	s = s+"#"+t;
 	
-	return 0;
+	vector<int> pi(sz(s)), ret(sz(t));
+	pi[0] = 0;
+	for(int i = 1; i < sz(s); i++){
+		int j = pi[i-1];
+		while(j and s[i] != s[j])
+			j = pi[j-1];
+		if(s[i] == s[j])
+			j++;
+		pi[i] = j;
+		
+		//if(i-sub-sub >= 0 and pi[i] == sub){
+		//	// substring t[i, i+pi) matches s
+		//	cout<<i-sub-sub<<' ';
+		//}
+	}
+	for(int i = 0; i < sz(t); i++)
+		ret[i] = pi[sub+1+i];
+	return ret;
 }

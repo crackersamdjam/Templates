324440fd5c821d422a93d56c011ceab8ff2ed749
Eric <crackersamdjam@gmail.com>
2020-08-17 14:15:40 -0400

    Update pseg

--- persistent_segment_tree.cpp
+++ persistent_segment_tree.cpp
@@ -1,4 +1,4 @@
-// https://dmoj.ca/problem/lrsubtree
+// https://dmoj.ca/problem/dmopc16c3p6
 
 #include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
@@ -10,118 +10,114 @@ template<typename First, typename ... Ints> void scan(First &arg, Ints&... rest)
 template<typename T> void print(T n){printn(n);pc(10);}
 template<typename First, typename ... Ints> void print(First arg, Ints... rest){printn(arg);pc(32);print(rest...);}
 
-#define lc (rt<<1)
-#define rc (rt<<1|1)
+#ifndef ONLINE_JUDGE
+template<typename T>
+void pr(T a){std::cerr<<a<<std::endl;}
+template<typename T,typename... Args>
+void pr(T a, Args... args) {std::cerr<<a<<' ',pr(args...);}
+#else
+template<typename... Args>
+void pr(Args... args){}
+#endif
+
 #define nm ((nl+nr)>>1)
 
 using namespace std;
-using ll = long long;
-using T = long long;
-using pii = pair<int, int>;
-const int MM = 5e5+5, LOG = 17, mod = 1e9+7, inf = 0x3f3f3f3f;
-const T DEF = 0;
+using T = int;
+const int MM = 1e5+5, NN = 5e5+5;
 
-int N, Q, a[MM];
-vector<int> g[MM], at_dep[MM];
-int tin[MM], tout[MM], timer, dep[MM], mx_dep;
+int n, q, ptr;
+T a[MM];
 
 struct node{
 	node *l, *r;
-	T sum;
-	node(T val = DEF){
+	T sum, pre, suf;
+	node(T val){
 		l = r = nullptr;
-		sum = val;
+		pre = suf = sum = val;
 	}
-	node(node *l, node *r): l(l), r(r){
-		if(!l) sum = r->sum;
-		else if(!r) sum = l->sum;
-		else sum = l->sum + r->sum;
+	node(node *_l, node *_r): l(_l), r(_r){   // merge function
+		sum = l->sum + r->sum;
+		pre = max(l->pre, l->sum + r->pre);
+		suf = max(r->suf, l->suf + r->sum);
 	}
-} *rt[MM];
+	node *copy(){
+		node *ret = new node(*this);
+		ret->l = ret->r = 0;
+		return ret;
+	}
+	~node(){
+		if(l) delete l;
+		if(r) delete r;
+	}
+} *rt[NN], *NDEF;
+
+void init(){ // initalize NOT DEFINED (does not exist) node
+	NDEF = new node(0);
+	NDEF->pre = NDEF->suf = INT_MIN/2;
+	NDEF->sum = 0;
+}
 
 node *build(int nl, int nr){
-	if(nl == nr) return new node(0);
+	if(nl == nr) return new node(a[nl]);
 	return new node(build(nl, nm), build(nm+1, nr));
 }
 
-node *upd(node *cur, int nl, int nr, int idx, ll val){
+node *update(node *cur, int nl, int nr, int idx, T val){
 	if(nl > idx || nr < idx) return cur;
 	if(nl == nr) return new node(val);
-	return new node(upd(cur->l, nl, nm, idx, val), upd(cur->r, nm+1, nr, idx, val));
+	return new node(update(cur->l, nl, nm, idx, val), update(cur->r, nm+1, nr, idx, val));
 }
 
-T query(node *cur, int nl, int nr, int l, int r){
-	if(nl > r || nr < l) return DEF;
-	else if(nl >= l && nr <= r) return cur->sum;
-	return query(cur->l, nl, nm, l, r) + query(cur->r, nm+1, nr, l, r);
+node *query(node *cur, int nl, int nr, int l, int r){
+	if(nl > r || nr < l) return NDEF->copy();
+	else if(nl >= l && nr <= r) return cur->copy();
+	return new node(query(cur->l, nl, nm, l, r), query(cur->r, nm+1, nr, l, r));
 }
 
-void dfs(int u, int p = -1){
-	tin[u] = ++timer;
-	at_dep[dep[u]].push_back(u);
-	mx_dep = max(mx_dep, dep[u]);
-	for (int v : g[u]) {
-		if (v == p) continue;
-		dep[v] = dep[u] + 1;
-		dfs(v, u);
-	}
-	tout[u] = timer;
+// debug segtree
+void out(node *cur, int nl, int nr){
+	pr(nl, nr, cur->sum, cur->pre, cur->suf);
+	if(nl == nr)
+		return;
+	out(cur->l, nl, nm); out(cur->r, nm+1, nr);
 }
 
-int main() {
-	ios_base::sync_with_stdio(0), cin.tie(0);
+int main(){
+	init();
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin>>n;
+	for(int i = 1; i <= n; i++)
+		cin>>a[i];
+	rt[ptr] = build(1, n);
 	
-	cin >> N >> Q;
-	assert(1 <= N && N <= 5e5);
-	assert(1 <= Q && Q <= 5e5);
-	for (int i = 1; i <= N; i++) {
-		cin >> a[i];
-		assert(0 <= a[i] && a[i] <= 1e9);
-	}
-	for (int i = 1, u, v; i < N; i++) {
-		cin >> u >> v;
-		assert(1 <= u && u <= N);
-		assert(1 <= v && v <= N);
-		g[u].push_back(v);
-		g[v].push_back(u);
-	}
-	dfs(1);
-	rt[0] = build(1, N);
-	for (int i = 0; i <= mx_dep; i++) {
-		if (i != 0) rt[i] = rt[i - 1];
-		for (int u : at_dep[i]) {
-			rt[i] = upd(rt[i], 1, N, tin[u], a[u]);
+	cin>>q;
+	for(int i = 1,x,y; i <= q; i++){
+		char op;
+		cin>>op;
+		if(op == 'U'){
+			cin>>x>>y;
+			rt[ptr+1] = update(rt[ptr], 1, n, x, y);
+			ptr++;
 		}
-	}
-	int cnt = 0;
-	ll ret = 0;
-	for (int i = 0; i < Q; i++) {
-		ll w, l, r;
-		cin >> w >> l >> r;
-		w ^= ret, l ^= ret, r ^= ret;
-		if (w < 1 || w > N) {
-			cerr << "bruh" << w << " " << l << " " << r << endl;
-			cerr << i << endl;
+		else if(op == 'G'){
+			cin>>x;
+			rt[ptr+1] = rt[x];
+			ptr++;
 		}
-		assert(1 <= w && w <= N);
-		assert(0 <= l && l <= N - 1);
-		assert(0 <= r && r <= N - 1);
-		assert(l <= r);
-		if (dep[w] + l > mx_dep) {
-			ret = 0;
-			cout << 0 << '\n';
-			continue;
+		else if(op == 'P'){
+			cin>>x>>y;
+			auto ret = query(rt[ptr], 1, n, x, y);
+			cout<<ret->pre<<'\n';
+			delete ret;
 		}
-		r = min(dep[w] + (int) r, mx_dep);
-		l = dep[w] + l - 1;
-		ll sum1 = query(rt[r], 1, N, tin[w], tout[w]);
-		ll sum2 = 0;
-		if (l >= dep[w]) sum2 = query(rt[l], 1, N, tin[w], tout[w]);
-		ret = sum1 - sum2;
-		cout << ret << '\n';
-		if (r != dep[w] && sum1 - sum2>0) cnt++;
+		else if(op == 'S'){
+			cin>>x>>y;
+			auto ret = query(rt[ptr], 1, n, x, y);
+			cout<<ret->suf<<'\n';
+			delete ret;
+		}
+		else abort();
 	}
-	cerr << cnt << endl;
-	
-	return 0;
 }

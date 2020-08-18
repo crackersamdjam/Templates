0d5e8035deddd2e59bf73d3184ee81adb4c025c7
Eric <crackersamdjam@gmail.com>
2020-08-18 18:54:37 -0400

    Update hld

--- hld.cpp
+++ hld.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define gc getchar_unlocked()
+#define pc(x) putchar_unlocked(x)
+template<typename T> int size(const T &x){return int(x.size());}
+template<typename T> void scan(T &x){x = 0;bool _=0;T c=gc;_=c==45;c=_?gc:c;while(c<48||c>57)c=gc;for(;c<48||c>57;c=gc);for(;c>47&&c<58;c=gc)x=(x<<3)+(x<<1)+(c&15);x=_?-x:x;}
+template<typename T> void printn(T n){bool _=0;_=n<0;n=_?-n:n;char snum[65];int i=0;do{snum[i++]=n%10+48;n/= 10;}while(n);--i;if (_)pc(45);while(i>=0)pc(snum[i--]);}
+template<typename First, typename ... Ints> void scan(First &arg, Ints&... rest){scan(arg);scan(rest...);}
+template<typename T> void print(T n){printn(n);pc(10);}
+template<typename First, typename ... Ints> void print(First arg, Ints... rest){printn(arg);pc(32);print(rest...);}
+
 using namespace std;
-typedef long long T;
+using T = long long;
+using L = long long;
 const int MM = 5e4+5;
 
 struct node{
-	T val, lp;
+	T val;
+	L lp;
 	int sz;
-	inline void apply(T v){
+	inline void apply(L v){
 		val += v*sz; //range sum
 		lp += v;
 	}
@@ -18,24 +30,25 @@ struct segtree{
 #define nm ((nl+nr)>>1)
 	node tree[MM*4];
 	const T DEF = 0;
+	const L LDEF = 0;
 	inline T merge(T va, T vb){ return va + vb;}
 	inline void pull(int rt){ tree[rt].val = merge(tree[lc].val, tree[rc].val);}
 	
 	// node with lazy val means yet to push to children (but updated itself)
 	inline void push(int rt, int nl, int nr){
-		T &val = tree[rt].lp;
+		L &val = tree[rt].lp;
 		if(nl != nr){
 			tree[lc].apply(val);
 			tree[rc].apply(val);
 		}
-		val = DEF;
+		val = LDEF;
 	}
 	
 	void build(int l = 0, int r = MM-1, int rt = 1){
 		int nl = l, nr = r;
 		
 		tree[rt].val = DEF;
-		tree[rt].lp = 0;
+		tree[rt].lp = LDEF;
 		tree[rt].sz = r-l+1;
 		
 		if(l == r)
@@ -45,7 +58,7 @@ struct segtree{
 		pull(rt);
 	}
 	
-	void update(int l, int r, T val, int nl = 0, int nr = MM-1, int rt = 1){
+	void update(int l, int r, L val, int nl = 0, int nr = MM-1, int rt = 1){
 		if(r < nl || l > nr)
 			return;
 		if(l <= nl && r >= nr){
@@ -124,7 +137,7 @@ T query(int a, int b){
 	return res;
 }
 
-void update(int a, int b, T v){
+void update(int a, int b, L v){
 	for(; head[a] != head[b]; b = par[head[b]]){
 		if(dep[head[a]] > dep[head[b]])
 			swap(a, b);
@@ -159,6 +172,4 @@ int main(){
 		else
 			cout<<query(a, b)<<'\n';
 	}
-	
-	return 0;
 }

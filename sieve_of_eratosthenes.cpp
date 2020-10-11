b8e34083694484e1b991e6a1fb072c5e440924a9
Kevin JY Cui <info@kevincui.dev>
2020-10-11 18:19:48 -0400

    Change to linear sieve

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
-const int N = 5000;
-bool sieve[N];
+const int N = 100001;
+int sieve[N];
+vector<int> primes;
 
 int main() {
-    memset(sieve, true, N); 
-    sieve[0] = false; sieve[1] = false;
+	memset(sieve, 0, N); 
 
-    for (int i=2; i<N; i++)
-        if (sieve[i])
-            for (int j=i*i; j<N; j+=i)
-                sieve[j] = false;
+	for (int i=2; i<N; i++) {
+		if (sieve[i] == 0) {
+			sieve[i] = i;
+			primes.push_back(i);
+		}
+		for (int j=0; j<(int)primes.size() && i*primes.at(j)<N; j++) {
+			sieve[i*primes.at(j)] = primes.at(j);
+			if (i%primes.at(j) == 0) break;
+		}
+	}
 	
-    return 0;
+	return 0;
 }

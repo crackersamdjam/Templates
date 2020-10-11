6158a9cba5d3ef41f2883e11f2f5906dffd37b7b
Kevin JY Cui <info@kevincui.dev>
2020-10-11 01:49:04 -0400

    Update sieve_of_eratosthenes.cpp

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-const int N = 502;
+const int N = 5000;
+bool sieve[N];
 
 int main() {
-
-    bool sieve[N]; memset(sieve, true, N); 
+    memset(sieve, true, N); 
     sieve[0] = false; sieve[1] = false;
 
     for (int i=2; i<N; i++)
         if (sieve[i])
             for (int j=i*i; j<N; j+=i)
                 sieve[j] = false;
-
+	
     return 0;
 }

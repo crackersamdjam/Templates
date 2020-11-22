3eafecbe426f601f15536b661c57c5e18454abf2
crackersamdjam <crackersamdjam@gmail.com>
2020-11-22 10:12:10 -0500

    Update SA

--- suffix_automaton.cpp
+++ suffix_automaton.cpp
@@ -1,85 +1,86 @@
 // overkill sol to https://atcoder.jp/contests/abc097/tasks/arc097_a
-// more functions https://github.com/bqi343/USACO/blob/master/Implementations/content/strings%20(14)/Heavy/SuffixAutomaton.h
+// more functions https://github.com/boi343/USACO/blob/master/Implementations/content/strings%20(14)/Heavy/SuffixAutomaton.h
 
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-struct st{
-	int len, link;
-	map<char, int> adj;
-};
-const int MM = 100002;
-st a[MM*2];
-int sz, last;
-ll cnt[MM*2];
-
-void init(){
-	a[0].len = 0;
-	a[0].link = -1;
-	sz++;
-	last = 0;
-}
-
-void extend(char c){
-	int cur = sz++;
-	a[cur].len = a[last].len + 1;
-	int p = last;
-	while (p != -1 && !a[p].adj.count(c)) {
-		a[p].adj[c] = cur;
-		p = a[p].link;
+struct SuffixAutomaton{
+	struct st{
+		int len, par;
+		ll cnt;
+		map<char, int> ch;
+	};
+	vector<st> v;
+	int sz, last;
+	
+	void init(int n){
+		v.resize(n*2);
+		v[0].len = 0; //0 is the root
+		v[0].par = -1;
+		sz = 1;
+		last = 0;
 	}
-	if (p == -1) {
-		a[cur].link = 0;
-	} else {
-		int q = a[p].adj[c];
-		if (a[p].len + 1 == a[q].len) {
-			a[cur].link = q;
-		} else {
-			int clone = sz++;
-			a[clone].len = a[p].len + 1;
-			a[clone].adj = a[q].adj;
-			a[clone].link = a[q].link;
-			while (p != -1 && a[p].adj[c] == q) {
-				a[p].adj[c] = clone;
-				p = a[p].link;
-			}
-			a[q].link = a[cur].link = clone;
+	
+	void extend(char c){
+		int cur = sz++;
+		v[cur].len = v[last].len+1;
+		int p = last;
+		while(p != -1 && !v[p].ch.count(c)){
+			v[p].ch[c] = cur;
+			p = v[p].par;
 		}
+		if(p == -1){
+			v[cur].par = 0;
+		}
+		else{
+			int o = v[p].ch[c]; //"other"
+			if(v[p].len+1 == v[o].len){
+				v[cur].par = o;
+			}
+			else{
+				int clone = sz++; //clone
+				v[clone].ch = v[o].ch;
+				v[clone].par = v[o].par;
+				v[clone].len = v[p].len+1;
+				while(p != -1 && v[p].ch[c] == o){
+					//redirect all these to clone
+					v[p].ch[c] = clone;
+					p = v[p].par;
+				}
+				v[o].par = v[cur].par = clone;
+			}
+		}
+		last = cur;
 	}
-	last = cur;
-}
-
-ll getsize(int x) {
-	if (cnt[x]) return cnt[x];
-	cnt[x] = 1;
-	for(auto i: a[x].adj){
-		cnt[x] += getsize(i.second);
+	
+	ll getsz(int x){
+		if(v[x].cnt)
+			return v[x].cnt;
+		for(auto i: v[x].ch)
+			v[x].cnt += getsz(i.second);
+		return ++v[x].cnt;
 	}
-	return cnt[x];
-}
-
-string kth(int cur, int k){
-	assert(k < cnt[cur]);
-	if(!k) return ""; //lexographically least
-	k--;
-	for(auto i: a[cur].adj){
-		if(k < cnt[i.second])
-			return i.first+kth(i.second, k);
-		k -= cnt[i.second];
+	
+	string kth(int cur, int k){
+		assert(k < v[cur].cnt);
+		if(!k) return ""; //lexographically least
+		k--;
+		for(auto i: v[cur].ch){
+			if(k < v[i.second].cnt)
+				return i.first+kth(i.second, k);
+			k -= v[i.second].cnt;
+		}
+		abort();
 	}
-	assert(0);
-	return "";
-}
+} SA;
 
 int main(){
 	string s; int k;
-	cin >> s >> k;
-	init();
+	cin>>s>>k;
+	SA.init(5000);
 	for(auto c: s)
-		extend(c);
-	getsize(0);
-	cout << kth(0, k);
-	
-	return 0;
+		SA.extend(c);
+	SA.getsz(0);
+	cout<<SA.kth(0, k)<<'\n';
 }

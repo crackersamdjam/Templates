4d4474bf2346f301d8aa1d4d77ae915d19faefd1
crackers <crackersamdjam@gmail.com>
2021-01-17 11:08:03 -0500

    Update line endings

--- content/math/ccw.cpp
+++ content/math/ccw.cpp
@@ -1,12 +1,12 @@
 template<typename T>
 inline pair<T,T> vec(pair<T,T> a, pair<T,T> b){
-    return make_pair(b.first-a.first,b.second-a.second);
+	return make_pair(b.first-a.first,b.second-a.second);
 }
 template<typename T>
 inline T cross(pair<T,T> a, pair<T,T> b){
-    return a.first*b.second-a.second*b.first;
+	return a.first*b.second-a.second*b.first;
 }
 template<typename T>
 inline bool ccw(pair<T,T> a, pair<T,T> b, pair<T,T> c){
-    return cross(vec(b,a),vec(b,c))<0;
+	return cross(vec(b,a),vec(b,c))<0;
 }
--- content/utils/data.sh
+++ content/utils/data.sh
@@ -21,10 +21,10 @@ t=1
 echo '- batched:' >> $name/init.yml
 for((v=20;v<=200000;i++,v*=10))
 do
-    echo $i $t
-    ./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
-    ./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
-    echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
+	echo $i $t
+	./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
+	./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
+	echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
 done
 echo '  points: 10' >> $name/init.yml
 
@@ -34,10 +34,10 @@ t=2
 echo '- batched:' >> $name/init.yml
 for((v=20;v<=200000;i++,v*=10))
 do
-    echo $i $t
-    ./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
-    ./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
-    echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
+	echo $i $t
+	./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
+	./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
+	echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
 done
 echo '  points: 10' >> $name/init.yml
 
@@ -47,10 +47,10 @@ t=3
 echo '- batched:' >> $name/init.yml
 for((v=20;v<=200000;i++,v*=10))
 do
-    echo $i $t
-    ./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
-    ./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
-    echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
+	echo $i $t
+	./gen $v $t > $name/data/$i.in || { echo 'gen run failed'; exit 1;}
+	./fast < $name/data/$i.in > $name/data/$i.out || { echo 'fast run failed'; exit 1;}
+	echo "  - {in: data/$i.in, out: data/$i.out}" >> $name/init.yml
 done
 echo '  points: 10' >> $name/init.yml
 

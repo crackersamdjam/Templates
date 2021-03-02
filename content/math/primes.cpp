c3077196f79a804381dc9915faae87aab52cacf3
crackers <crackersamdjam@gmail.com>
2021-03-01 19:23:20 -0500

    Add more number theoretic algorithms

--- content/math/primes.cpp
+++ content/math/primes.cpp
@@ -1,15 +1,7 @@
 #include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
-#define gc getchar()
-#define pc(x) putchar(x)
-template<typename T> void scan(T &x){x = 0;bool _=0;T c=gc;_=c==45;c=_?gc:c;while(c<48||c>57)c=gc;for(;c<48||c>57;c=gc);for(;c>47&&c<58;c=gc)x=(x<<3)+(x<<1)+(c&15);x=_?-x:x;}
-template<typename T> void printn(T n){bool _=0;_=n<0;n=_?-n:n;char snum[65];int i=0;do{snum[i++]=char(n%10+48);n/= 10;}while(n);--i;if (_)pc(45);while(i>=0)pc(snum[i--]);}
-template<typename First, typename ... Ints> void scan(First &arg, Ints&... rest){scan(arg);scan(rest...);}
-template<typename T> void print(T n){printn(n);pc(10);}
-template<typename First, typename ... Ints> void print(First arg, Ints... rest){printn(arg);pc(32);print(rest...);}
 
 using namespace std;
-using ll = __int128;
 
 //mt19937_64 g(chrono::steady_clock::now().time_since_epoch().count());
 //mt19937_64 g((uint64_t) new char);
@@ -19,10 +11,12 @@ long long randl(long long l, long long r){return uniform_int_distribution<long l
 
 template<class T> T gcd(T _a, T _b){return _b == 0 ? _a : gcd(_b, _a%_b);}
 template<class T, class U> T fpow(T _base, U _pow, T _mod){_base %= _mod; T _x = 1; for(; _pow > 0; _pow >>= 1){ if(_pow&1) _x = _x*_base%_mod; _base = _base*_base%_mod;} return _x;}
+template<class T> T mulmod(T _a, T _b, T _mod){ return _a%_mod*(_b%_mod)%_mod;}
 template<class T> T divmod(T _a, T _b, T _mod){return _a*fpow(_b, _mod-2, _mod)%_mod;}
 
-// O(iterations * complexity of fpow)
-template<class T> bool isprime(T N, int iterations = 7){
+// https://github.com/wesley-a-leung/Resources/blob/master/Content/C++/math/Primes.h#L94
+// checks whether N is prime in O(iterations * complexity of fpow)
+template<class T> bool miller_rabin(T N, int iterations = 7){
 	if(N < 2 or N%6%4 != 1)
 		return (N|1) == 3;
 	vector<T> A = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
@@ -43,10 +37,12 @@ template<class T> bool isprime(T N, int iterations = 7){
 	return 1;
 }
 
+// count number of divisors of N in O(N^(1/3))
 template<class T> T count_divisors(T N){
-	ll ret = 1, m = cbrtl(N);
-	for(ll i = 2; i <= m; i++){
-		ll cnt = 0;
+	T ret = 1;
+	T m = cbrtl(N);
+	for(T i = 2; i <= m; i++){
+		T cnt = 0;
 		while(N%i == 0){
 			N /= i;
 			cnt++;
@@ -55,10 +51,10 @@ template<class T> T count_divisors(T N){
 	}
 	// N is now either 1, p, pq, or p^2
 	if(N > 1){
-		if(isprime(N))
+		if(miller_rabin(N))
 			ret *= 2;
 		else{
-			ll p = sqrtl(N);
+			T p = sqrtl(N);
 			if(p*p == N) ret *= 3;
 			else ret *= 4;
 		}
@@ -96,28 +92,113 @@ template<class T> pair<vector<T>, vector<T>> linear_sieve(T N){
 	return {move(low), move(primes)};
 }
 
+// https://github.com/wesley-a-leung/Resources/blob/master/Content/C++/math/Primes.h#L267
+// Returns an arbitrary divisor of N in O(N^(1/4))
+template<class T> T pollards_rho(T N, int iterations = 40){
+	if(N == 1) return 1;
+	auto f = [&](T x){
+		return mulmod(x, x, N) + 1;
+	};
+	T x = 0, y = 0, p = 2, q;
+	int t = 0, i = 1;
+	while(t++ % iterations || gcd(p, N) == 1){
+		if(x == y)
+			y = f(x = ++i);
+		if(q = mulmod(p, max(x, y) - min(x, y), N))
+			p = q;
+		x = f(x); y = f(f(y));
+	}
+	return gcd(p, N);
+}
+
+// https://github.com/wesley-a-leung/Resources/blob/master/Content/C++/math/Primes.h#L296
+// prime factorizes x in O(x^(1/4) log(x))
+template<class T> vector<T> pollards_rho_prime_factor(T x, int pollardsRhoIters = 40, int millerRabinIters = 7){
+	if(x == 1) return {};
+	vector<T> ret;
+	queue<T> q; q.push(x);
+	while(size(q)){
+		T y = q.front(); q.pop();
+		if(miller_rabin(y, millerRabinIters))
+			ret.push_back(y);
+		else{
+			q.push(pollards_rho(y, pollardsRhoIters));
+			q.push(y/q.back());
+		}
+	}
+	return ret;
+}
+
+namespace factors2{
+	//https://cboj.ca/problem/factor2
+	using ll = long long;
+	int main(){
+		 ll n;
+		 cin>>n;
+		 cout<<count_divisors(n)<<'\n';
+		 return 0;
+	}
+}
+
+namespace phantom1{
+	//https://dmoj.ca/problem/phantom1
+	using ll = long long;
+	int main(){
+		 // auto v = linear_sieve((ll)1e6).first;
+		 // auto v = fast_sieve((ll)1e6).first;
+		 auto a = linear_sieve((ll)1e6).second;
+		 int q;
+		 cin>>q;
+		 while(q--){
+			int l, r, ans = 0;
+			cin>>l>>r;
+			// for(int i = l; i < r; i++)
+				// ans += v[i] == i;
+			// for(int i = l; i < r; i++)
+				// ans += v[i];
+			for(auto i: a)
+				ans += i >= l and i < r;
+			cout<<ans<<'\n';
+		 }
+		 return 0;
+	}
+}
+
+namespace boring{
+	// https://ccoj.ca/problem/boring
+	using ll = long long;
+	int main(){
+		ll a, b;
+		cin>>a>>b;
+		auto v = pollards_rho_prime_factor(__gcd(a, b));
+		
+		ll ans = 1, sub = 0, last = 0, cnt = 0;
+		sort(all(v));
+		v.emplace_back(-1);
+		for(ll p: v){
+			if(p == last){
+				cnt++;
+			}
+			else{
+				sub += last;
+				ll f = 1, add = ans;
+				while(cnt--){
+					f = f*last;
+					ans += f*add;
+				}
+				
+				last = p;
+				cnt = 1;
+			}
+		}
+		ans -= sub;
+		cout<<ans<<'\n';
+		return 0;
+	}
+}
+
 int main(){
-	// test primality checks on https://cboj.ca/problem/factor2
-	// ll n;
-	// scan(n);
-	// print(count_divisors(n));
-	
-	
-	// test sieves on https://dmoj.ca/problem/phantom1
-	// auto v = linear_sieve((__int128)1e6).first;
-	// auto v = fast_sieve((__int128)1e6).first;
-	// auto a = linear_sieve((__int128)1e6).second;
-	// int q;
-	// scan(q);
-	// while(q--){
-	// 	int l, r, ans = 0;
-	// 	scan(l, r);
-	// 	// for(int i = l; i < r; i++)
-	// 		// ans += v[i] == i;
-	// 	// for(int i = l; i < r; i++)
-	// 		// ans += v[i];
-	// 	for(auto i: a)
-	// 		ans += i >= l and i < r;
-	// 	cout<<ans<<'\n';
-	// }
+	// factors2::main();
+	// phantom1::main();
+	boring::main();
 }

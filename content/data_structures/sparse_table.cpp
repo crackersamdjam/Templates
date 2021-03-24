58087d9d05848966a36957dc922330fd1a411e3f
crackersamdjam <crackersamdjam@gmail.com>
2021-03-24 12:54:21 -0400

    Update sparse table

--- content/data_structures/sparse_table.cpp
+++ content/data_structures/sparse_table.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-const int MM = 2002;
 
-struct sparse_table{
-	int n; ll sp[12][MM];
-	ll query(int a, int b){
-		int k = 31-__builtin_clz(b-a+1);
+template<class T> struct sparse_table{
+	int n; vector<vector<T>> sp;
+	T query(int l, int r){
+		int k = __lg(b-a+1);
 		print(max(sp[k][a], sp[k][b-(1<<k)+1]));
 	}
-	void build(int _n){
-		n = _n;
-		for(int k = 1; k <= 31-__builtin_clz(n); k++)
+	void build(vector<T> v){
+		n = size(v);
+		sp.resize(__lg(n));
+		for(int k = 1; k <= __lg(n); k++){
+			sp.resize(n);
 			for(int i = 0; i+(1<<k)-1 <= n; i++)
 				sp[k][i] = max(sp[k-1][i], sp[k-1][i+(1<<(k-1))]);
+		}
 	}
 };
+// make sure to test this later

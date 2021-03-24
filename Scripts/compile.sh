1ae11c5f5429553a3a5559513fa7dfe8b9c4a8e0
crackers <crackersamdjam@gmail.com>
2021-03-23 20:45:57 -0400

    Test

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -6,18 +6,18 @@ total=0
 pass=0
 fail=0
 for i in $(seq 2 $numArgs); do
-  test=${!i}
-  total += 1
-  echo ""
-  echo "$test:"
-  $1 $test
-  retCode=$?
-  if (($retCode == 0)); then
-    echo "Compiled Successfully"
-    pass += 1
-  else
-    fail += 1
-  fi
+	test=${!i}
+	total+=1
+	echo ""
+	echo "$test:"
+	$1 $test
+	retCode=$?
+	if (($retCode == 0)); then
+		echo "Compiled Successfully"
+		pass+=1
+	else
+		fail+=1
+	fi
 done
 
 echo ""
@@ -26,5 +26,5 @@ echo "$pass successful"
 echo "$fail failed"
 
 if (($fail != 0)); then
-  exit 1
+	exit 1
 fi

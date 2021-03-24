628cdd907eb12dfcbc35d847d9f9deb2d915a145
crackersamdjam <crackersamdjam@gmail.com>
2021-03-24 12:00:45 -0400

    Make gp_hash_table.h actually compile

--- content/data_structures/gp_hash_table.h
+++ content/data_structures/gp_hash_table.h
@@ -1,9 +1,7 @@
-#include <bits/stdc++.h>
+#include <bits/extc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 
-struct chash {
+struct chash{
 	const int RANDOM = (long long)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
 	static unsigned long long hash_f(unsigned long long x) {
 		x += 0x9e3779b97f4a7c15;
@@ -15,11 +13,5 @@ struct chash {
 	int operator()(int x) const { return hash_f(x)^RANDOM; }
 };
 
-gp_hash_table<int, int, chash> mp;
-
-
-
-//#include <ext/pb_ds/assoc_container.hpp>
-__gnu_pbds::gp_hash_table<ll, __gnu_pbds::null_type> mp;
-// acts like a set
-// mp.find(x) != mp.end() to check
+// __gnu_pbds::gp_hash_table<int, __gnu_pbds::null_type, chash> mp;
+// <key, value, optional custom hash>

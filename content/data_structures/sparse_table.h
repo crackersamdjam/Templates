664d21cf066489478329ca74b64c6c915c5ab1b0
crackersamdjam <crackersamdjam@gmail.com>
2021-04-01 17:39:16 -0400

    Fix sparse table

--- content/data_structures/sparse_table.h
+++ content/data_structures/sparse_table.h
@@ -6,16 +6,17 @@ template<class T> struct sparse_table{
 	int n; vector<vector<T>> sp;
 	T query(int l, int r){
 		int k = __lg(r-l+1);
-		print(max(sp[k][l], sp[k][r-(1<<k)+1]));
+		return min(sp[k][l], sp[k][r-(1<<k)+1]);
 	}
 	void build(vector<T> v){
 		n = size(v);
-		sp.resize(__lg(n));
-		for(int k = 1; k <= __lg(n); k++){
-			sp.resize(n);
-			for(int i = 0; i+(1<<k)-1 <= n; i++)
-				sp[k][i] = max(sp[k-1][i], sp[k-1][i+(1<<(k-1))]);
+		sp.resize(__lg(n)+1);
+		sp[0] = v;
+		for(int i = 1; i <= __lg(n); i++){
+			sp[i].resize(n, -1);
+			for(int j = 0; j+(1<<i)-1 < n; j++){
+				sp[i][j] = min(sp[i-1][j], sp[i-1][j+(1<<(i-1))]);
+			}
 		}
 	}
 };
-// make sure to test this later
